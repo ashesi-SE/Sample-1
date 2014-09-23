502fa44e1ac74a4135f3382b63e7ba5e0e604f11
toddwseattle <todd@warrenfamily.org>
2014-09-23 08:15:41 +0000

    still resolving references with mainview

--- app/mainview/mainview.js
+++ app/mainview/mainview.js
@@ -5,14 +5,14 @@ angular.module('myApp.mainview', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/mainview', {
     templateUrl: 'mainview/mainview.html',
-    controller: 'ManViewCtrl'
+    controller: 'MainViewCtrl'
   });
 }])
 
 .controller('MainViewCtrl', function($scope) {
 	$scope.cedidollarrate=3.25;
-	$scope.cedidollar=f() { return cedidollarrate; }
-	$scope.dollarcedi=f() { return (1/cedidollarrate); }
+	$scope.cedidollar=f() { return cedidollarrate; };
+	$scope.dollarcedi=f() { return (1/cedidollarrate); };
 
 
 });
