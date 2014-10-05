f4aa5c146b1180e798067156d5087de1214dece2
toddwseattle <todd@warrenfamily.org>
2014-10-05 12:09:43 +0000

    added way to record /latest into /rates

--- node/server.js
+++ node/server.js
@@ -14,7 +14,25 @@ var CURRENCY= {
 				complete(body);
 			}
 		}); //request
-	  } // get latest
+	  }, // get latest
+	GetUTCDTFilename : function(cd){
+		// constructs a filename of type YYYY-M-D!H-M-S
+		return(cd.getUTCFullYear().toString()+"-"+(cd.getUTCMonth()+1).toString()+"-"+cd.getUTCDate().toString()+
+			   "!"+cd.getUTCHours().toString()+"-"+cd.getUTCMinutes().toString()+"-"+cd.getUTCSeconds().toString());
+	},
+	WriteCurrencyFile: function(exchangetext){
+				var curr=JSON.parse(exchangetext);
+				console.log("timestamp:"+curr.timestamp+" type:"+typeof(curr.timestamp));
+
+				var currtime=new Date(Date(curr.timestamp));
+				console.log("currtime:"+currtime+" type:"+typeof(currtime));
+				var currtimefile=this.GetUTCDTFilename(currtime);
+				console.log("Writing to"+"rates\\"+currtimefile+".json");
+				fs.writeFile("rates\\"+currtimefile+".json",exchangetext, function (err) {
+							  if (err) throw err;
+							  console.log('It\'s saved!');
+							});
+		}  //WriteCurrencyFile
 	};
   http.createServer(function(request, response) {
 		  console.log("URI: "+request.url);
@@ -22,7 +40,9 @@ var CURRENCY= {
 		  {
 			  	CURRENCY.GetLatest(function(body){
 	 			response.writeHead(200, {"Content-Type": "text/plain"});
+
 				response.write(body);
+			  	CURRENCY.WriteCurrencyFile(body);
 			  	response.end();
 		  	});
 		  } else {
